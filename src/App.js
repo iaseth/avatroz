31a7b2d99d1d47361903df4d847d7d84bf7d57dd
Ankur Seth <iankurseth@gmail.com>
2022-02-10 20:43:38 +0530

    c0 with cra, tailwind and cleanup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+
+function App () {
+	return (
+		<div className="App">
+			<div className="min-h-screen flex bg-purple-400 text-white">
+				<div className="text-4xl m-auto">
+					<a href="https://github.com/iaseth/avatroz" className="bg-purple-500 px-6 py-3 duration-300 hover:bg-purple-600">Avatroz</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
